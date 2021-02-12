1bdd903a6086bd76c9ee5f2ff670df55e353318f
unknown <davyberra@gmail.com>
2021-02-11 19:39:22 -0800

    changed to whitenoise v.3.2.1

--- davys_issue_tracker/deploy.py
+++ davys_issue_tracker/deploy.py
@@ -9,10 +9,10 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 
 import os
 
-from whitenoise import WhiteNoise
+from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'davys_issue_tracker.deploy_settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root='issue_tracker/static/issue_tracker')
+application = DjangoWhiteNoise(application)
