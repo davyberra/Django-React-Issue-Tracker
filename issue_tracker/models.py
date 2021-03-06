3fb3eb3b62df89478b8f4c2951bd576f5f5bcbf1
unknown <davyberra@gmail.com>
2021-03-06 08:51:57 -0800

    added 'issue_type' to Issue model

--- issue_tracker/models.py
+++ issue_tracker/models.py
@@ -18,11 +18,17 @@ class Issue(models.Model):
     HIGH = 'High'
     MEDIUM = 'Medium'
     LOW = 'Low'
+    BUG = 'Bug'
+    FEATURE = 'Feature'
     PRIORITY_CHOICES = [
         (CRITICAL, 'Critical'),
         (HIGH, 'High'),
         (MEDIUM, 'Medium'),
         (LOW, 'Low')
+    ]
+    ISSUE_TYPE_CHOICES = [
+        (BUG, 'Bug'),
+        (FEATURE, 'Feature'),
     ]
     issue_text = models.TextField()
     date_posted = models.DateTimeField('date_posted')
@@ -31,6 +37,11 @@ class Issue(models.Model):
         max_length=8,
         choices=PRIORITY_CHOICES,
         default=LOW,
+    )
+    issue_type = models.CharField(
+        max_length=7,
+        choices=ISSUE_TYPE_CHOICES,
+        default=BUG,
     )
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     project = models.ForeignKey(Project, on_delete=models.SET_NULL, null=True, blank=True)
