fd07b838ea9b59305a9a4573949f74a96fcf1f55
unknown <davyberra@gmail.com>
2021-07-05 14:48:34 -0700

    fixed dir settings

--- davys_issue_tracker/settings.py
+++ davys_issue_tracker/settings.py
@@ -87,7 +87,7 @@ CORS_ORIGIN_WHITELIST = (
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'frontend/build')],
+        'DIRS': [os.path.join(BASE_DIR / 'frontend/build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
