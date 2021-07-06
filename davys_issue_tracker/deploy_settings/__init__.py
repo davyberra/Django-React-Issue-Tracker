2717985abb8ad08b15073c7492097fa51167a8f2
unknown <davyberra@gmail.com>
2021-07-06 10:04:26 -0700

    added dj-database-url module

--- davys_issue_tracker/deploy_settings/__init__.py
+++ davys_issue_tracker/deploy_settings/__init__.py
@@ -1,5 +1,5 @@
 from davys_issue_tracker.settings import *
-
+import dj_database_url
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -11,7 +11,7 @@ ALLOWED_HOSTS = [
 
 SECRET_KEY = get_env_variable("SECRET_KEY")
 DATABASE_URL = get_env_variable("DATABASE_URL")
-# db_from_env = dj_database_url.config()
-DATABASES['default'].update(DATABASE_URL)
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
