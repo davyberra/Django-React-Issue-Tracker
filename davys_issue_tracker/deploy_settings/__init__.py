9c682faaa66d9c2e5859bae3f29cb023617cbcfe
unknown <davyberra@gmail.com>
2021-07-05 18:56:53 -0700

    fixed database_url env variable

--- davys_issue_tracker/deploy_settings/__init__.py
+++ davys_issue_tracker/deploy_settings/__init__.py
@@ -1,4 +1,3 @@
-import dj_database_url
 from davys_issue_tracker.settings import *
 
 
@@ -11,8 +10,8 @@ ALLOWED_HOSTS = [
 ]
 
 SECRET_KEY = get_env_variable("SECRET_KEY")
-
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+DATABASE_URL = get_env_variable("DATABASE_URL")
+# db_from_env = dj_database_url.config()
+# DATABASES['default'].update(db_from_env)
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
